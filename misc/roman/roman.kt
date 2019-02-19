e3fb179006245087ea95289972130be382c873b0
Sean K. Roth <therothinator@gmail.com>
2019-02-18 17:01:57 -0800

    don't need to use map for this

--- misc/roman/roman.kt
+++ misc/roman/roman.kt
@@ -1,25 +1,25 @@
-val romans = mapOf(
-    1000 to "M",
-    900 to "CM",
-    500 to "D",
-    400 to "CD",
-    100 to "C",
-    90 to "XC",
-    50 to "L",
-    40 to "XL",
-    10 to "X",
-    9 to "IX",
-    5 to "V",
-    4 to "IV",
-    1 to "I"
-).toSortedMap(Comparator { a, b -> b - a})
+val romans = listOf(
+    Pair(1000, "M"),
+    Pair(900, "CM"),
+    Pair(500, "D"),
+    Pair(400, "CD"),
+    Pair(100, "C"),
+    Pair(90, "XC"),
+    Pair(50, "L"),
+    Pair(40, "XL"),
+    Pair(10, "X"),
+    Pair(9, "IX"),
+    Pair(5, "V"),
+    Pair(4, "IV"),
+    Pair(1, "I")
+)
 
 fun Int.toRoman(): String {
     var remainder = this
     val ret = mutableListOf<String>()
 
     while (remainder != 0) {
-        romans.toList().find { p -> p.first <= remainder }?.let { largest ->
+        romans.find { p -> p.first <= remainder }?.let { largest ->
             ret.add(largest.second)
             remainder -= largest.first
         }
